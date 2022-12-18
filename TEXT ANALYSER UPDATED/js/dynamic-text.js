d870ef65ae7bb0029682f11fcbae67ecd74a2a92
Akshata sarde <chikkysarde@gmail.com>
2022-12-18 18:28:12 +0530

    Add files via upload

---
+++ TEXT ANALYSER UPDATED/js/dynamic-text.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const dynamicContent = document.getElementById("dynamic-text");
+    console.log(dynamicContent);
+    
+    const phrases = ["Text Analyser...", "Text Analyser...", "Text Analyser..."];
+    let phraseIndex = 0;
+    let letterIndex = 0;
+    const typingSpeed = 100;
+    const erasingSpeed = 75;
+    
+    function printLetters(phrase) {
+    
+        if(letterIndex == phrase.length) {
+            // clear letter which have been typed
+            clearLetters();
+        }
+    
+        else if(letterIndex < phrase.length) {
+            dynamicContent.textContent += phrase.charAt(letterIndex);
+            letterIndex += 1;
+            
+            setTimeout(function() {
+                printLetters(phrase)
+            }, typingSpeed)
+        }
+    
+    }
+    
+    function clearLetters() {
+        if(letterIndex == -1) {
+            phraseIndex = (phraseIndex + 1) % phrases.length;
+            letterIndex = 0;
+            printLetters(phrases[phraseIndex]);
+        }
+    
+        else if(letterIndex > -1) {
+            let updatedPhrase = "";
+            for(let index = 0; index < letterIndex; index++) {
+                updatedPhrase += phrases[phraseIndex].charAt(index);
+            }
+    
+            console.log(updatedPhrase);
+            dynamicContent.textContent = updatedPhrase;
+            letterIndex -= 1;
+            setTimeout(clearLetters, erasingSpeed);
+        }
+    }
+    
+    printLetters(phrases[phraseIndex]);
+    
+});
